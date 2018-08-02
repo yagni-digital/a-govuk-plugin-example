87ab3586aacc2c3f61f4dc1f2730f0508fa2e8b5
Mat Carey <mat+yagni-digital@matcarey.co.uk>
2018-08-02 09:52:28 +0100

    Adding an example showing the full functionality of the hooks.

--- b/c/fun.js
+++ b/c/fun.js
@@ -1,1 +1,9 @@
-window.confirm('Are you having fun?')
+
+window.MY_NAMESPACE = window.MY_NAMESPACE || {}
+
+window.MY_NAMESPACE.demoFeature = function (selector) {
+  window.$(selector).on('click', function (e) {
+    e.preventDefault();
+    window.alert('The behaviour from a-govuk-plugin-example is used - a kitten');
+  })
+}
